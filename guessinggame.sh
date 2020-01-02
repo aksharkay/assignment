8ef8d8bfcdc649f586f8069c5c54e15bb7be011d
aksharkay <aksharkomm@gmail.com>
2020-01-02 11:49:04 +0530

    added files to assignment

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,25 @@
-c=0
-
-echo "How many files are in the current directory?"
-
-function input
+function check
 {
-	read guess
+	while [ $c -ne 1 ]
+	do
+		input
+
+		if [ $guess -eq "3" ]
+		then
+			c=1
+			echo "Congratulations! Your guess is correct."
+
+		elif [ $guess -lt "3" ]
+		then
+			echo -e "Your guess is too low.\nPlease try again:"
+
+		else
+			echo -e "Your guess is too high.\nPlease try again:"
+		fi
+	done
 }
 
-while [ $c -ne 1 ]
-do
-	input
-
-	if [ $guess -eq "3" ]
-	then
-		c=1
-		echo "Congratulations! Your guess is correct."
-
-	elif [ $guess -lt "3" ]
-	then
-		echo -e "Your guess is too low.\nPlease try again:"
-
-	else
-		echo -e "Your guess is too high.\nPlease try again:"
-	fi
-done
+c=0
+echo "How many files are in the current directory?"
+read guess
+check
